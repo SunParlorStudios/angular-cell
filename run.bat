3dc8e6216a07b694bf5f305d40f111c6b21540af
Rens Verspagen <rens_verspagen@hotmail.com>
2015-04-21 00:09:28 +0200

    sefasef

--- run.bat
+++ run.bat
@@ -1,1 +1,1 @@
-snuffbox.exe -console true -reload true -src_directory "REPLACEDEZESHIT" -editMode true
+snuffbox.exe -console true -reload true -src_directory "C:/Users/Stanyzone/Desktop/angler-cell" -editMode true
