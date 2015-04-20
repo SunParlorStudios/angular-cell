5c42109065fd02109521a41e00e444d7b6f9f4d7
Daniël Konings <konings_304@hotmail.com>
2015-04-20 22:44:28 +0200

    Fixed snuffbox memory issues

--- js/gameplay/laser.js
+++ js/gameplay/laser.js
@@ -62,6 +62,7 @@ _.extend(Laser.prototype, {
 
 	update: function (blocks, dt)
 	{
+		return;
 		if (this._endPoint !== false)
 		{
 			var angle = Math.atan2(this._position.y - this._endPoint.pos.y, this._position.x - this._endPoint.pos.x);
